5a729a3690bb153e0531546e49e27d8f42cb252e
Jonathan Frankel <github@habanerojam.xyz>
2023-10-29 07:31:40 +0800

    removed commented out config, changed a few keymaps, added tealdeer

--- Configs/.config/hypr/scripts/volumecontrol.sh
+++ Configs/.config/hypr/scripts/volumecontrol.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env sh
 
-ScrDir=`dirname $(realpath $0)`
-source $ScrDir/globalcontrol.sh
-
+ScrDir=$(dirname $(realpath "$0"))
+. "$ScrDir"/globalcontrol.sh
 
 # define functions
 
-function print_error
-{
-cat << "EOF"
+print_error() {
+	cat <<"EOF"
     ./volumecontrol.sh -[device] <action>
     ...valid device are...
         i -- [i]nput decive
@@ -20,58 +18,65 @@ cat << "EOF"
 EOF
 }
 
-function notify_vol
-{
-    vol=`pamixer $srce --get-volume | cat`
-    angle="$(( (($vol+2)/5) * 5 ))"
-    ico="${icodir}/vol-${angle}.svg"
-    bar=$(seq -s "." $(($vol / 15)) | sed 's/[0-9]//g')
-    dunstify "t2" -a "$vol$bar" "$nsink" -i $ico -r 91190 -t 800
+notify_vol() {
+	vol=$(pamixer "$srce" --get-volume | cat)
+	angle=$((((vol + 2) / 5) * 5))
+	ico="${icodir}/vol-${angle}.svg"
+	bar=$(seq -s "." $((vol / 15)) | sed 's/[0-9]//g')
+	dunstify $vol -a "$vol$bar" "$nsink" -i "$ico" -r 91190 -t 800
 }
 
-function notify_mute
-{
-    mute=`pamixer $srce --get-mute | cat`
-    if [ "$mute" == "true" ] ; then
-        dunstify "t2" -a "muted" "$nsink" -i ${icodir}/muted-${dvce}.svg -r 91190 -t 800
-    else
-        dunstify "t2" -a "unmuted" "$nsink" -i ${icodir}/unmuted-${dvce}.svg -r 91190 -t 800
-    fi
+notify_mute() {
+	mute=$(pamixer "$srce" --get-mute | cat)
+	if [ "$mute" = "true" ]; then
+		dunstify "t2" -a "muted" "$nsink" -i "${icodir}"/muted-"${dvce}".svg -r 91190 -t 800
+	else
+		dunstify "t2" -a "unmuted" "$nsink" -i "${icodir}"/unmuted-"${dvce}".svg -r 91190 -t 800
+	fi
 }
-
 
 # set device source
 
-while getopts io SetSrc
-do
-    case $SetSrc in
-    i) nsink=$(pamixer --list-sources | grep "_input." | head -1 | awk -F '" "' '{print $NF}' | sed 's/"//')
-        srce="--default-source"
-        dvce="mic" ;;
-    o) nsink=$(pamixer --get-default-sink | grep "_output." | awk -F '" "' '{print $NF}' | sed 's/"//')
-        srce=""
-        dvce="speaker" ;;
-    esac
+while getopts io SetSrc; do
+	case $SetSrc in
+	i)
+		nsink=$(pamixer --list-sources | grep "_input." | head -1 | awk -F '" "' '{print $NF}' | sed 's/"//')
+		srce="--default-source"
+		dvce="mic"
+		;;
+	o)
+		nsink=$(pamixer --get-default-sink | grep "_output." | awk -F '" "' '{print $NF}' | sed 's/"//')
+		srce=""
+		dvce="speaker"
+		;;
+	*)
+		echo ""
+		;;
+	esac
 done
 
-if [ $OPTIND -eq 1 ] ; then
-    print_error
+if [ $OPTIND -eq 1 ]; then
+	print_error
 fi
-
 
 # set device action
 
-shift $((OPTIND -1))
+shift $((OPTIND - 1))
 step="${2:-5}"
-icodir="~/.config/dunst/icons/vol"
+icodir="$HOME/.config/dunst/icons/vol"
 
 case $1 in
-    i) pamixer $srce -i ${step}
-        notify_vol ;;
-    d) pamixer $srce -d ${step}
-        notify_vol ;;
-    m) pamixer $srce -t
-        notify_mute ;;
-    *) print_error ;;
+i)
+	pamixer "$srce" -i "${step}"
+	notify_vol
+	;;
+d)
+	pamixer "$srce" -d "${step}"
+	notify_vol
+	;;
+m)
+	pamixer "$srce" -t
+	notify_mute
+	;;
+*) print_error ;;
 esac
-
